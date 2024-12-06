618770c6315146f0a4f4fad052c6b105e753cd23
Nilushka-Poornima <nilushkapoornima169@gmail.com>
2024-12-06 15:17:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden text-neutral-300 antialiased selection:bg-white/20 selection:text-cyan-50zz">
+      {/* Background */}
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-slate-900 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(176,31,238,0.41),rgba(255,255,255,0))]"></div>
+      </div>
+
+      {/* Main Content */}
+      <div className="container mx-auto lg:px-20 px-6">
+        <Navbar />
+        <Home />
+        <About />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { motion } from "framer-motion";
+import profilePic from "../assets/kevinRushProfile.jpg";
+import { HERO_CONTENT } from "../constants/index";
+
+const Home = () => {
+  return (
+    <div className="sm:flex sm:flex-col sm:items-center lg:flex lg:flex-row  md:gap-3 ">
+      {/* Left Section: Name and Title */}
+      <div className="flex flex-col lg:items-start  lg:text-left lg:flex-grow  ">
+        <div className="flex flex-col items-center text-justify lg:flex lg:flex-col lg:items-center w-full">
+          <div className="border-b-2 relative flex flex-col items-center lg:items-start py-5 border-purple-950">
+
+            <h1 className="text-4xl lg:text-6xl font-thin pb-8 ">
+              {/* First Name with Zoop Animation */}
+              <motion.span
+                whileHover={{
+                  scale: 1.1,
+                  transition: { duration: 0.4, ease: "easeInOut" },
+                  translateY: -8,
+                }}
+                className="mr-2 text-white inline-block cursor-pointer"
+              >
+                Nilushka
+              </motion.span>
+              {/* Last Name with Zoop Animation */}
+              <motion.span
+                whileHover={{
+                  scale: 1.1,
+                  translateY: -8,
+                  transition: { duration: 0.4, ease: "easeInOut" },
+                }}
+                className="text-white inline-block cursor-pointer"
+              >
+                Poornima
+              </motion.span>
+            </h1>
+            <span className="bg-gradient-to-r from-pink-400 via-slate-500 to-purple-500 bg-clip-text text-2xl lg:text-4xl tracking-tight text-transparent">
+              Front-end Developer
+            </span>
+          </div>
+          {/* Paragraph Section */}
+          <p className="py-2 mt-8 text-sm lg:text-base sm:text-base text-gray-300 max-w-sm lg:max-w-lg lg:text-justify sm:text-center">
+            {HERO_CONTENT}
+          </p>
+
+        </div>
+      </div>
+
+      {/* Right Section: Profile Image */}
+      <div className="flex flex-col items-center w-full lg:w-auto lg:mr-16">
+        <img
+          src={profilePic}
+          alt="Profile"
+          className="w-32 h-32 lg:w-96 lg:h-96 rounded-3xl object-cover shadow-lg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
