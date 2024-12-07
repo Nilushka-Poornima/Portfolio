d1d2aa6f1116bf3bbc24383bcbc8d42ce3015da6
Nilushka-Poornima <nilushkapoornima169@gmail.com>
2024-12-07 22:27:12 +0530

    experience page and Technologies page is done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import { ABOUT_TEXT } from '../constants'
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 ">
-      <h1 className="mt-24 mb-28 text-center text-5xl">About
+      <h1 className="mt-24 lg:mb-28 p-10  text-center text-5xl">About
         <span className="text-neutral-500"> me</span>
       </h1>
       <div className="flex flex-wrap sm:flex-wrap-reverse ">
@@ -16,7 +16,7 @@ const About = () => {
         </div>
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:justify-start">
-            <p className="py-2 mt-8 text-sm lg:text-base  text-gray-300 max-w-sm lg:max-w-lg text-center lg:text-justify border-b-2 border-purple-950 m-3">
+            <p className="py-2 mt-8 text-sm lg:text-base  text-gray-300 max-w-sm lg:max-w-lg text-justify border-b-2 border-purple-950 m-3">
               {ABOUT_TEXT}
             </p>
           </div>
